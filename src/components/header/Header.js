12442b8b52f78dcdb92c27d5b7eccacbbe28f00f
Ahmar Jabbar <87254977+ahmar7@users.noreply.github.com>
2022-08-19 21:36:08 +0500

    header added

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../../assets/css/style.css"
+const Header = () => {
+    return (
+        <div className='main-container'>
+            <div className="header">
+
+            </div>
+            
+        </div>
+    );
+}
+
+export default Header;
