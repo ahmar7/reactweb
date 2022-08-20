b024dc8d3695ade669418ced5657504b065abe74
Ahmar Jabbar <87254977+ahmar7@users.noreply.github.com>
2022-08-20 12:09:52 +0500

    plan update

--- src/components/plan/Plan.js
+++ src/components/plan/Plan.js
@@ -12,11 +12,37 @@ const Plan = () => {
           <button className="btn2">YEARLY</button>
         </div>
         <div className="plan-block">
-          <div className="p-block">
+          <div className="p-block pt2">
             <h3>BASIC</h3>
             <img src={Pblock1} alt="" />
+            <ul>
+              <li> &nbsp; &nbsp;Store Catalog</li>
+              <li> &nbsp; &nbsp;Linked with Whatsapp Catalog</li>
+              <li> &nbsp; &nbsp;Linked with Facebook Catalog</li>
+              <li> &nbsp; &nbsp;Upload up to 10 products</li>
+              <li> &nbsp; &nbsp;Payment Gateway</li>
+              <li> &nbsp; &nbsp;Chat Automation</li>
+              <li> &nbsp; &nbsp;Order Management</li>
+            </ul>
+            <button>GET STARTED</button>
           </div>
-          <div className="p-block"></div>
+          <div className="p-block ">
+            <div className="pop">
+              <h3>POPULAR</h3>
+            </div>
+            <h3>STANDARD</h3>
+            <img src={Pblock2} alt="" />
+            <ul>
+              <li> &nbsp; &nbsp;Store Catalog</li>
+              <li> &nbsp; &nbsp;Linked with Whatsapp Catalog</li>
+              <li> &nbsp; &nbsp;Linked with Facebook Catalog</li>
+              <li> &nbsp; &nbsp;Upload up to 10 products</li>
+              <li> &nbsp; &nbsp;Payment Gateway</li>
+              <li> &nbsp; &nbsp;Chat Automation</li>
+              <li> &nbsp; &nbsp;Order Management</li>
+            </ul>
+            <button>GET STARTED</button>
+          </div>
         </div>
       </div>
     </div>
