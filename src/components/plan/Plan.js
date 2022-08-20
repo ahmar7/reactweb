271636cc13da67b0c5ed453dfec29683223a2e9e
Ahmar Jabbar <87254977+ahmar7@users.noreply.github.com>
2022-08-20 11:23:37 +0500

    plan update

--- src/components/plan/Plan.js
+++ src/components/plan/Plan.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import Pblock1 from "../../assets/images/block1.png";
+import Pblock2 from "../../assets/images/block2.png";
 const Plan = () => {
   return (
     <div className="container">
@@ -9,6 +10,13 @@ const Plan = () => {
         <div className="planbtn">
           <button className="btn1">MONTHLY</button>
           <button className="btn2">YEARLY</button>
+        </div>
+        <div className="plan-block">
+          <div className="p-block">
+            <h3>BASIC</h3>
+            <img src={Pblock1} alt="" />
+          </div>
+          <div className="p-block"></div>
         </div>
       </div>
     </div>
