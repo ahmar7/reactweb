8b8907673596e54d657ffbeb510e30293496c478
Ahmar Jabbar <87254977+ahmar7@users.noreply.github.com>
2022-08-20 10:10:16 +0500

    header update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import "../../assets/css/style.css"
+import React from "react";
+import "../../assets/css/style.css";
+import Logo from "../../assets/images/logo.png";
 const Header = () => {
-    return (
-        <div className='container'>
-            <div className="header">
-
-            </div>
-            
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="header-column-left">
+          <img src={Logo} alt="" />
         </div>
-    );
-}
+        <div className="header-column-right">
+          <ul className="nav-li">
+            <li>Home</li>
+            <li>Contact</li>
+            <li className="active">Sign up</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
