37fde8dc9a077aa78c9a5cab76ed35d2d73116ef
Ahmar Jabbar <87254977+ahmar7@users.noreply.github.com>
2022-08-20 09:17:23 +0500

    home update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import Header from './components/header/Header'
 import Home from './components/home/Home';
-import HomeIcon from './assets/images/home-icon.png'
 function App() {
   return (
     <div className="App">
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import "../../assets/css/style.css"
+import React from "react";
+import "../../assets/css/style.css";
+import HomeIcon from "../../assets/images/home-icon.png";
+
 const Home = () => {
-    return (
-      <div className="container">
-        <div className="block">
-          <div className="column-left">
-            <h1>SELL EASILY ON SOCIAL MEDIA</h1>
-            <p>
-              Simplify the way you receive & manage orders, engage with
-              customers, facilitate payments & much more
-            </p>
-          </div>
-          <div className="column-right"></div>
+  return (
+    <div className="container">
+      <div className="block">
+        <div className="column-left">
+          <h1>
+            SELL EASILY ON <br /> SOCIAL MEDIA
+          </h1>
+          <p>
+            Simplify the way you <br /> receive & manage orders, <br /> engage
+            with customers, <br /> facilitate payments & <br /> much more
+          </p>
+          <button>LEARN MORE</button>
+        </div>
+        <div className="column-right">
+          <img src={HomeIcon} alt="" />
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Home;
